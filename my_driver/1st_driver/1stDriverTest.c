46716652f11c5c50b4187170e912e239891e363e
grant <zhuimengcanyang@163.com>
2015-11-05 15:16:02 +0800

    增加了LED的驱动，以及测试程序。

--- my_driver/1st_driver/1stDriverTest.c
+++ my_driver/1st_driver/1stDriverTest.c
@@ -9,8 +9,29 @@
 int main(int argc, char * argv[])
 {
     int fd = 0;
+    char value = 0;
+    
     fd = open("/dev/led", O_RDWR);
+    if(fd <0)
+    {
+        printf("can't open file \n");
+    }
 
+    
+    if(argc != 2)
+    {
+        printf("Usage: \n");
+        printf("%s on|off\n", argv[0]);
+        return 0;
+    }
+
+    if(strcmp(argv[1], "on") == 0)
+        value = 1;
+    else 
+        value = 0;
+
+    write(fd, &value, sizeof(value));
+    
     return 0;
 }
 
--- my_driver/1st_driver/1st_driver.c
+++ my_driver/1st_driver/1st_driver.c
@@ -6,6 +6,7 @@
 #include <asm/io.h>
 #include <linux/device.h>
 
+#include <linux/uaccess.h>
 
 //#include <linux/init.h>
 //#include <linux/module.h>
@@ -21,14 +22,31 @@
 
 
 
+static volatile unsigned long * gpfcon;
+static volatile unsigned long * gpfdat;
 
 static struct class *first_driver_class;
 static struct class_device *first_class_device;
 
 
+#define LED_CONFIG()    do{                                                             \
+                            *gpfcon &= ~((0x3<<(4*2)) | (0x3<<(5*2)) | (0x3<<(6*2)));	\ 
+                            *gpfcon |= ((0x1<<(4*2)) | (0x1<<(5*2)) | (0x1<<(6*2)));   \
+                        }while(0)
+
+        
+#define ALL_LED_ON()    (*gpfdat &= ~((1<<4) | (1<<5) | (1<<6)))
+#define ALL_LED_OFF()   (*gpfdat |= ((1<<4) | (1<<5) | (1<<6)))
+
 static int first_driver_open(struct inode *inode, struct file *filp)
 {
-    printk("first_driver_open\n");
+    //printk("first_driver_open\n");
+    
+    	/* ÅäÖÃGPF4,5,6ÎªÊä³ö */
+//	*gpfcon &= ~((0x3<<(4*2)) | (0x3<<(5*2)) | (0x3<<(6*2)));
+//	*gpfcon |= ((0x1<<(4*2)) | (0x1<<(5*2)) | (0x1<<(6*2)));
+	LED_CONFIG();
+	
     return 0;
 }
 
@@ -36,6 +54,22 @@ static int first_driver_open(struct inode *inode, struct file *filp)
 static ssize_t first_driver_write(struct file * file, const char * buf, size_t count, loff_t *off)
 {
 	ssize_t retval = count;
+    int val = 0xff;
+    unsigned long len = 0;
+    
+    len = copy_from_user(&val, buf, count);
+
+    if(val == 1)
+    {
+        // turn on LEDs
+        ALL_LED_ON();
+    }
+    else
+    {
+        // turn off LEDs
+        ALL_LED_OFF();
+
+    }
 
 	return retval;
 }
@@ -60,6 +94,12 @@ static int first_driver_init(void)
 	first_driver_class = class_create(THIS_MODULE, "first_driver");
 	//class_device_create(first_driver_class, NULL, MKDEV(major, 0), first_class_device, "led");
 	first_class_device = class_device_create(first_driver_class, NULL, MKDEV(major, 0), NULL, "led");
+
+    // add LED gpio initialize, remap GPIO to kernel.
+    // 16: indicate 16 bytes, remap 4 registers(32bits width).
+	gpfcon = (volatile unsigned long *)ioremap(0x56000050, 16);
+	gpfdat = gpfcon + 1;
+
 
     printk("first_driver_init\n");
     
